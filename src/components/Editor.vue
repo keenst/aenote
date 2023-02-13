b94c79d46339a9252cd02f9901940bad92aa3b48
aeSpike <spike.z.vitalis@gmail.com>
2023-02-13 20:41:07 +0000

    very basic editor structure

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,3 +1,40 @@
 <template>
-    <div>Editor</div>
-</template>
+    <div>
+      <p v-for="(id, index) in formattedIds" :key="index">
+        {{ id.body }} id:{{ id.id }} type:{{ id.type }}
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        ids: [],
+        format: []
+      };
+    },
+    computed: {
+      formattedIds() {
+        return this.format.map(id => {
+          return this.ids.find(i => i.id === id);
+        });
+      }
+    },
+    created() {
+      Promise.all([
+        fetch("/ids.json")
+          .then(response => response.json())
+          .then(data => {
+            this.ids = data;
+          }),
+        fetch("/format.json")
+          .then(response => response.json())
+          .then(data => {
+            this.format = data;
+          })
+      ]);
+    }
+  };
+  </script>
+  
