0a33cb9525af1e1c91c1470147f4ff0293571345
keenst <leo.siven@icloud.com>
2023-02-18 19:25:15 +0100

    Fixed incorrect amount of spaces on indents

--- src/components/ElementIterator.vue
+++ src/components/ElementIterator.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-for="item in localFormat">
-    {{ "&nbsp;".repeat(depth * 3) }}
+    {{ "&nbsp;".repeat(depth == 0 ? 0 : (depth * 4) - 1) }}
     <Element :id="item.id"/>
     <div v-if="item.children.length" v-for="item2 in item.children">
       <ElementIterator :localFormat="item.children" :depth="depth + 1"/>
